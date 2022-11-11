d98b19e54eac99211f5dd2f00f8b1c9c9fe8dadc
Ambi9u0us <xw_151130@163.com>
2022-11-10 21:59:30 -0800

    Update RoleManagement.vue

--- src/components/page/RoleManagement.vue
+++ src/components/page/RoleManagement.vue
@@ -7,18 +7,18 @@
         </div>
         <div class="container">
             <div class="inContainer">
-                <el-table :data="data" border class="table" ref="multipleTable" @selection-change="handleSelectionChange">
+                <el-table :data="data" border class="table" ref="multipleTable">
                 <el-table-column prop="jobNum" label="工号" sortable width="300" align="center">
                 </el-table-column>
                 <el-table-column prop="username" label="姓名" width="300" align="center">
                 </el-table-column>
-                <el-table-column prop="role" label="角色" :formatter="formatter" align="center">
+                <el-table-column prop="role" label="角色" align="center">
                 </el-table-column>
                 </el-table>
             </div> 
             
             <div class="pagination">
-                    <el-pagination background @current-change="handleCurrentChange" layout="prev, pager, next" :total="1000">
+                    <el-pagination background @current-change="handleCurrentChange" layout="prev, pager, next" :total="100">
                     </el-pagination>
             </div>
         </div>
@@ -45,8 +45,8 @@
                 this.getData(val);
             },
             async getData(val){
-                const{data:res} =await this.$http.get("alarm",{params:{page:val,pageSize:this.pageSize}})
-                console.log(res)
+                const{data:res} =await this.$http.get("user/role",{params:{page:val,pageSize:this.pageSize}})
+                // console.log(res)
                 if(res.code===200){
                     this.data=[];
                     if(res.data.length===0){
@@ -54,7 +54,14 @@
                     }
                     else{
                         for (let i=0;i<res.data.length;i++){
-                            this.data.push(res.data[i]);
+                            var user={jobNum:res.data[i].jobNum,username:res.data[i].username,role:""}
+                            if(res.data[i].role===1){
+                                user.role="管理员"
+                            }
+                            else{
+                                user.role="普通用户"
+                            }
+                            this.data.push(user);
                         }
                     }
                     
